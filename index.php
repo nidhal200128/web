39c158b2080efc961225e3b28f310344e52abfa8
nidhal200128 <91366911+nidhal200128@users.noreply.github.com>
2021-09-25 09:15:31 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <script language="javascript" src="index.js"></script>
+    </head> 
+    <body>
+<h1>Inscription d'un nouveau client :</h1>
+<label for="a1">Nom</label><br>
+<input type="text" id="a1" placeholder="saisir votre nom" name="a"><br><br>
+
+<label for="a2">prénom</label><br>
+<input type="text" id="a2" placeholder="saisir votre prenom" name="b"><br><br>
+
+<label for="a3">addresse email</label><br>
+<input type="text" id="a3" placeholder="saisir adresse email" name="c"><br><br>
+
+
+<label for="a6">votre addresse</label><br>
+<input type="text" id="a6" placeholder="saisir votre adresse" name="x"><br><br>
+
+
+
+
+<label for="a4">mot de passe</label><br>
+<input type="password" id="a4" placeholder="saisir votre mot de passe" name="d"><br><br>
+
+<label for="a5">confirmation mot de passe</label><br>
+<input type="password" id="5" placeholder="resaisr votre mdp" name="e"><br><br>
+
+<input type="submit" name="b1" value="inscrire" onclick="verif()" ><br><br>
+
+<?php
+echo ("inscription terminer");
+?> 
+
+<a href="C:\Users\nidha\OneDrive\Documents\Nidhal Boumeiza\Examen_TP" target="_blank">commander quelque chose</a>
+    </body>
+
+</html>
